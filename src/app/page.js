a5ccb2c2ab8c58c8e3d2214b9afc144409c704c7
n4ryn <vinaykumar.cs19@aryacollege.in>
2025-06-06 23:45:59 +0530

    Fix build issue

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,5 @@
 import Questions from "@/components/Questions";
-import Services from "@/components/services";
+import Services from "@/components/Services";
 
 export default function Home() {
   return (
